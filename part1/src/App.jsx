0457a7d50b7a7c2e57f456d1a19e8d0dd65d889e
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-22 16:39:29 +0200

    Tutorial useState left right

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-    console.log('rendering with counter value', counter)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
-  const decreaseByOne = () => { 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={decreaseByOne} text='minus' />
-      <Button onClick={setToZero} text='zero' />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
 }
