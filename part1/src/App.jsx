36511721902df4c8d89373145710dd259f3dce1f
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-22 11:55:43 +0200

    Ej 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,12 +2,24 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
-const Content = (props) => {
-  return <>
-    <p>{props.part1}: {props.exercises1}</p>
-    <p>{props.part2}: {props.exercises2}</p>
-    <p>{props.part3}: {props.exercises3}</p>
-  </>
+const Part = (Part) => {
+  return (
+    <div>
+      <p>
+        {Part.name} {Part.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <Part name="Fundamentals of React" exercises="10" />
+      <Part name="Using props to pass data" exercises="7" />
+      <Part name="State of a component" exercises="14" />
+    </div>
+  )
 }
 
 const Total = (props) => {
@@ -21,17 +33,14 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Content  />
       <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   )
