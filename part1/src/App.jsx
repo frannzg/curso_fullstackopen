f76eb98391aac463909dd60cb8f5efc5088f2c7e
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-22 14:22:23 +0200

    Ej 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,23 @@
-const course = 'Half Stack application development'
-
-const parts = [
-  { name: 'Fundamentals of React', exercises: 10 },
-  { name: 'Using props to pass data', exercises: 7 },
-  { name: 'State of a component', exercises: 14 }
-]
+ const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>
+  return <h1>{course.name}</h1>
 }
 
 const Part = ({ name, exercises }) => {
@@ -22,14 +32,14 @@ const Content = ({ parts }) => {
   return (
     <div>
       {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
+        <Part key={index} name={course.parts[index].name} exercises={course.parts[index].exercises} />
       ))}
     </div>
   )
 }
 
-const Total = ({ parts }) => {
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+const Total = () => {
+  const totalExercises = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
   return (
     <p>
       Number of exercises <b>{totalExercises}</b>
@@ -41,8 +51,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts.exercises} />
     </div>
   )
 }
