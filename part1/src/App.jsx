3a8fde2a2d151d0e21b767fc70b3820ffeb991a5
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-24 15:32:01 +0200

    Ej 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,6 +8,14 @@ const Display = ({ good, neutral, bad }) => (
   </div>
 )
 
+const DisplayStatistics = ({ all, average, positive }) => (
+  <div>
+    <p>all {all}</p>
+    <p>average {average}</p>
+    <p>positive {positive} %</p>
+  </div>
+)
+
 const Button = ({ handleClick, text }) => (
   <button onClick ={handleClick}>{text}</button>
 )
@@ -16,7 +24,11 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -25,6 +37,10 @@ const App = () => {
       <Button handleClick={ () => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
       <Display good={good} neutral={neutral} bad={bad} />
+      {all === 0 
+        ? <p>No feedback given</p>
+        : <DisplayStatistics all={all} average={average} positive={positive.toFixed(1)} />
+      }
     </div>
   )
 }
