3d49de42c95fad9bc0c8e633ec5cbbe952d28127
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-24 15:40:54 +0200

    Ej 1.8 + Ej 1.9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 
-const Display = ({ good, neutral, bad }) => (
-  <div>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-  </div>
-)
 
-const DisplayStatistics = ({ all, average, positive }) => (
-  <div>
-    <p>all {all}</p>
-    <p>average {average}</p>
-    <p>positive {positive} %</p>
-  </div>
-)
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive.toFixed(1)} %</p>
+    </div>
+  )
+
+}
 
 const Button = ({ handleClick, text }) => (
   <button onClick ={handleClick}>{text}</button>
@@ -24,11 +31,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
-  
+    
   return (
     <div>
       <h1>give feedback</h1>
@@ -36,11 +39,7 @@ const App = () => {
       <Button handleClick={ () => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={ () => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Display good={good} neutral={neutral} bad={bad} />
-      {all === 0 
-        ? <p>No feedback given</p>
-        : <DisplayStatistics all={all} average={average} positive={positive.toFixed(1)} />
-      }
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
