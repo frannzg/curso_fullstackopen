6f5325604b6304a1b7fad7e494de58d14d402cf2
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-24 17:30:11 +0200

    Teorica par2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,109 +1,27 @@
-import { useState } from 'react'
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad
-  const average = all === 0 ? 0 : (good - bad) / all
-  const positive = all === 0 ? 0 : (good / all) * 100
-
-  if (all === 0) {
-    return <p>No feedback given</p>
-  }
-
-  return (
-    <div>
-      <table>
-      <tbody>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average.toFixed(1)} />
-        <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
-      </tbody>
-    </table>
-    </div>
-  )
-
-}
-
-const Button = ({ handleClick, text }) => (
-  <button onClick ={handleClick}>{text}</button>
-)
-
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
-
-  const handleNextAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length)
-    setSelected(randomIndex)
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
   }
 
-  const handleVote = () => {
-    const copy = [...votes]      // hacemos una copia
-    copy[selected] += 1          // sumamos un voto
-    setVotes(copy)               // actualizamos el estado
-  }
-
-  // Encontrar la anécdota con más votos
-  const maxVotes = Math.max(...votes)
-  const topAnecdoteIndex = votes.indexOf(maxVotes)
-    
-  return (
-    <div>
-      <div>
-        <h1>give feedback</h1>
-        <Button handleClick={ () => setGood(good + 1)} text="good" />
-        <Button handleClick={ () => setNeutral(neutral + 1)} text="neutral" />
-        <Button handleClick={ () => setBad(bad + 1)} text="bad" />
-        <h1>statistics</h1>
-        <Statistics good={good} neutral={neutral} bad={bad} /> 
-      </div>
-      <div>
-        <h1>Anecdote</h1>
-        <p>{anecdotes[selected]}</p>
-        <p>has {votes[selected]} votes</p>
-        <button onClick={handleVote}>vote</button>
-        <button onClick={handleNextAnecdote}>next anecdote</button> 
-         <h1>Anecdote with most votes</h1>
-        {maxVotes === 0 ? (
-          <p>No votes yet</p>
-        ) : (
-          <>
-            <p>{anecdotes[topAnecdoteIndex]}</p>
-            <p>has {maxVotes} votes</p>
-          </>
-        )} 
-      </div>
-
-    </div>
-
-    
-  )
+  return <Course course={course} />
 }
 
 export default App
