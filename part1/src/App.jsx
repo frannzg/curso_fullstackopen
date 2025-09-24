2c36fd59785381385374c7208d525036451be434
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-24 16:03:46 +0200

    Ej 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -40,9 +40,26 @@ const Button = ({ handleClick, text }) => (
 )
 
 const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
     
   return (
     <div>
@@ -51,8 +68,15 @@ const App = () => {
       <Button handleClick={ () => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={ () => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} /> 
+      <div>
+        <p>{anecdotes[selected]}</p>
+        <button onClick={handleNextAnecdote}>next anecdote</button>
+      </div>
+
     </div>
+
+    
   )
 }
 
