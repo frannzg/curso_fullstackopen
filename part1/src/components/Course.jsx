06f8147d8da5aa1f105ac401f32d8f6b0e7ae1e3
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-25 13:58:24 +0200

    Ej 2.1

---
+++ part1/src/components/Course.jsx
@@ -0,0 +1,26 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
