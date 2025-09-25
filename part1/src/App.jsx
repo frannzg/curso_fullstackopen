ef0539bfc523cf1faac23e50070fda99dbfaa423
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-25 14:52:14 +0200

    Ej 2.9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -17,7 +22,8 @@ const App = () => {
     } else {
       const personObject = { 
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(personObject))
     }
@@ -26,17 +32,24 @@ const App = () => {
     setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  // Filtra según el valor del filtro (ignora mayúsculas/minúsculas)
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with: <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h3>Add a new</h3>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -49,10 +62,10 @@ const App = () => {
         </div>
       </form>
 
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map((person, i) => (
-          <li key={i}>
+        {personsToShow.map(person => (
+          <li key={person.id}>
             {person.name} — {person.number}
           </li>
         ))}
