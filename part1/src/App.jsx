edea3c7dcfb711dae83df48a41d95702ef73868a
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-25 14:49:07 +0200

    Ej 2.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,28 +2,36 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-   const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
 
-    // Verificar si el nombre ya existe
     const nameExists = persons.some(person => person.name === newName)
 
     if (nameExists) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      const personObject = { name: newName }
+      const personObject = { 
+        name: newName,
+        number: newNumber
+      }
       setPersons(persons.concat(personObject))
     }
 
-    setNewName('') // limpiar input
+    setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -34,17 +42,19 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
 
-      {/* debug temporal */}
-      <div>debug: {newName}</div>
-
       <h2>Numbers</h2>
       <ul>
         {persons.map((person, i) => (
-          <li key={i}>{person.name}</li>
+          <li key={i}>
+            {person.name} — {person.number}
+          </li>
         ))}
       </ul>
     </div>
