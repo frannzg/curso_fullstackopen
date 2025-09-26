53b5bc1943f9b4c1d03abc02641714d5b5b819f3
Fran Gómez <frangogoinfo17@gmail.com>
2025-09-26 12:13:29 +0200

    Ej 2.13

--- Exercice/src/App.jsx
+++ Exercice/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+//import axios from 'axios'
 import PersonForm from './components/PersonForm'
+import personService from './services/persons.js'
 
 const Filter = ({ filter, onFilterChange }) => (
   <div>
@@ -30,10 +31,10 @@ export default function App() {
 
   // Cargar datos iniciales
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
       .catch(error => {
         alert(`Error al cargar los contactos: ${error.message}`)
@@ -56,33 +57,33 @@ export default function App() {
 
     if (existing) {
       if (window.confirm(`${newName} ya existe. ¿Quieres actualizar el número?`)) {
-        axios
-          .put(`http://localhost:3001/persons/${existing.id}`, personObject)
-          .then(response => {
-            setPersons(persons.map(p => 
-              p.id !== existing.id ? p : response.data
-            ))
-            setNewName('')
-            setNewNumber('')
-          })
-          .catch(error => {
-            alert(`Error al actualizar el contacto: ${error.message}`)
-          })
+        personService
+        .updated(existing.id, personObject)
+        .then(returnedPerson => {
+          setPersons(persons.map(p =>
+            p.id !== existing.id ? p : returnedPerson
+          ))
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error => {
+          alert(`Error al actualizar el contacto: ${error.message}`)
+        })
       }
       return
     }
 
-    axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
       .catch(error => {
         alert(`Error al crear el contacto: ${error.message}`)
       })
-  }
+    }
 
   const personsToShow = persons.filter(p =>
     p.name.toLowerCase().includes(filter.toLowerCase())
